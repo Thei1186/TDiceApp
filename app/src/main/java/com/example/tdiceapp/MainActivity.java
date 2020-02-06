4dbf0d8d660ae7180601166c0813be79f41b2bcd
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2020-02-06 12:44:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tdiceapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.tdiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_roll;
+    LinearLayout dice_container;
+    ImageView dice1, dice2;
+    Random rng;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rng = new Random();
+
+        dice1 = findViewById(R.id.iv_dice1);
+        dice2 = findViewById(R.id.iv_dice2);
+        dice1.setImageResource(R.drawable.one);
+        dice2.setImageResource(R.drawable.two);
+        dice_container = findViewById(R.id.LL_dice_container);
+        btn_roll = findViewById(R.id.btn_roll_dice);
+        btn_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollFirstDie();
+            }
+        });
+    }
+
+    private void rollFirstDie() {
+        rollSecondDie();
+        int firstRoll = rng.nextInt(6) + 1;
+        switch (firstRoll)
+        {
+            case 1:
+                dice1.setImageResource(R.drawable.one);
+                break;
+            case 2:
+                dice1.setImageResource(R.drawable.two);
+                break;
+            case 3:
+                dice1.setImageResource(R.drawable.three);
+                break;
+            case 4:
+                dice1.setImageResource(R.drawable.four);
+                break;
+            case 5:
+                dice1.setImageResource(R.drawable.five);
+                break;
+            case 6:
+                dice1.setImageResource(R.drawable.six);
+                break;
+        }
+    }
+
+    private void rollSecondDie() {
+        int secondRoll = rng.nextInt(6) + 1;
+        switch (secondRoll)
+        {
+            case 1:
+                dice2.setImageResource(R.drawable.one);
+                break;
+            case 2:
+                dice2.setImageResource(R.drawable.two);
+                break;
+            case 3:
+                dice2.setImageResource(R.drawable.three);
+                break;
+            case 4:
+                dice2.setImageResource(R.drawable.four);
+                break;
+            case 5:
+                dice2.setImageResource(R.drawable.five);
+                break;
+            case 6:
+                dice2.setImageResource(R.drawable.six);
+                break;
+        }
+
+    }
+}
